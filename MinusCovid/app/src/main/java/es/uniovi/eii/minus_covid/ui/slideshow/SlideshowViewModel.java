723f639090a1025c6e2722b82c09c73a73b8e174
Iván <uo265368@uniovi.es>
2020-11-03 11:22:02 +0100

    Navigation Drawer Template

---
+++ MinusCovid/app/src/main/java/es/uniovi/eii/minus_covid/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package es.uniovi.eii.minus_covid.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
