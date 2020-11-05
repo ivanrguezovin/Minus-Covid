6519592aa2be84a3423e9b8fce47d662b7058650
Iván <uo265368@uniovi.es>
2020-11-05 11:40:42 +0100

    Dialog añadido al salir

--- MinusCovid/app/src/main/java/es/uniovi/eii/minus_covid/ui/salir/ExitFragment.java
+++ MinusCovid/app/src/main/java/es/uniovi/eii/minus_covid/ui/salir/ExitFragment.java
@@ -1,5 +1,7 @@
 package es.uniovi.eii.minus_covid.ui.salir;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,13 +9,50 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import es.uniovi.eii.minus_covid.R;
+import es.uniovi.eii.minus_covid.ui.mapa.MapViewModel;
 
 public class ExitFragment extends Fragment {
 
+    private MapViewModel mapViewModel;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        System.exit(0);
-        return null;
+        mapViewModel =
+                ViewModelProviders.of(this).get(MapViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_map, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+        mapViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        AlertDialog dialogo = new AlertDialog
+            .Builder(getActivity()) // NombreDeTuActividad.this, o getActivity() si es dentro de un fragmento
+            .setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Hicieron click en el botón positivo, así que la acción está confirmada
+                    System.exit(0);
+                }
+            })
+            .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Hicieron click en el botón negativo, no confirmaron
+                    // Simplemente descartamos el diálogo
+                    dialog.dismiss();
+                }
+            })
+            .setMessage("¿Deseas salir de la aplicación?") // El mensaje
+            .create();// No olvides llamar a Create, ¡pues eso crea el AlertDialog!
+        dialogo.show();
+        return root;
     }
 }
