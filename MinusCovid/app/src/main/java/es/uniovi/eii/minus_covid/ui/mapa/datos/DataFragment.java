370f14745aef6ef9acf285299d33e6a707e1767c
Iván <uo265368@uniovi.es>
2020-11-06 21:32:18 +0100

    FragmentData

---
+++ MinusCovid/app/src/main/java/es/uniovi/eii/minus_covid/ui/mapa/datos/DataFragment.java
@@ -0,0 +1,23 @@
+package es.uniovi.eii.minus_covid.ui.mapa.datos;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import es.uniovi.eii.minus_covid.R;
+
+public class DataFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_data, container, false);
+        return root;
+    }
+}
