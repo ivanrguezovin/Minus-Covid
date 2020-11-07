5fb1fa71a77487d305a013ad7b744b69b659d88d
Luis Carlos <uo257335@uniovi.es>
2020-11-07 13:54:41 +0100

    Parser hecho

--- MinusCovid/app/src/main/java/es/uniovi/eii/minus_covid/util/ComunidadDto.java
+++ MinusCovid/app/src/main/java/es/uniovi/eii/minus_covid/util/ComunidadDto.java
@@ -15,6 +15,16 @@ public class ComunidadDto {
     public int nuevos_casos; //today_new_confirmed
     public int nuevos_curados; //today_new_recovered
     public int nuevos_fallecidos; //today_new_deaths
-    public int nuevos_uci; //today_new_invensive_care
+    public int nuevos_uci; //today_new_intensive_care
+
+    @Override
+    public String toString() {
+        return "ComunidadDto [nombre=" + nombre + ", total_casos_acumulado=" + total_casos_acumulado
+                + ", total_curados_acumulado=" + total_curados_acumulado + ", total_infectados_acumulado="
+                + total_infectados_acumulado + ", total_fallecidos_acumulado=" + total_fallecidos_acumulado
+                + ", total_uci_acumulado=" + total_uci_acumulado + ", nuevos_casos=" + nuevos_casos
+                + ", nuevos_curados=" + nuevos_curados + ", nuevos_fallecidos=" + nuevos_fallecidos + ", nuevos_uci="
+                + nuevos_uci + "]";
+    }
     
 }
