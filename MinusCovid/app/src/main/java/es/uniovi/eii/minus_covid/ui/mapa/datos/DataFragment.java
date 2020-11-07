35c5d9fe37a735f30e74edf35203b188a51bf28f
Iván <uo265368@uniovi.es>
2020-11-07 15:56:16 +0100

    Dto es parcelable

--- MinusCovid/app/src/main/java/es/uniovi/eii/minus_covid/ui/mapa/datos/DataFragment.java
+++ MinusCovid/app/src/main/java/es/uniovi/eii/minus_covid/ui/mapa/datos/DataFragment.java
@@ -39,31 +39,31 @@ public class DataFragment extends Fragment {
         dto = datosRecuperados.getParcelable("dto");
 
         totalCasos = root.findViewById(R.id.totalCasos);
-        totalCasos.setText(dto.total_casos_acumulado);
+        totalCasos.setText(Integer.toString(dto.total_casos_acumulado));
 
         totalCurados = root.findViewById(R.id.totalCurados);
-        totalCurados.setText(dto.total_curados_acumulado);
+        totalCurados.setText(Integer.toString(dto.total_curados_acumulado));
 
         totalInfectados = root.findViewById(R.id.totalInfectados);
-        totalInfectados.setText(dto.total_infectados_acumulado);
+        totalInfectados.setText(Integer.toString(dto.total_infectados_acumulado));
 
         totalFallecidos = root.findViewById(R.id.totalFallecidos);
-        totalFallecidos.setText(dto.total_fallecidos_acumulado);
+        totalFallecidos.setText(Integer.toString(dto.total_fallecidos_acumulado));
 
         totalEnUCI = root.findViewById(R.id.totalEnUCI);
-        totalEnUCI.setText(dto.total_uci_acumulado);
+        totalEnUCI.setText(Integer.toString(dto.total_uci_acumulado));
 
         nuevosCasos = root.findViewById(R.id.nuevosCasos);
-        nuevosCasos.setText(dto.nuevos_casos);
+        nuevosCasos.setText(Integer.toString(dto.nuevos_casos));
 
         nuevosCurados = root.findViewById(R.id.nuevosCurados);
-        nuevosCurados.setText(dto.nuevos_curados);
+        nuevosCurados.setText(Integer.toString(dto.nuevos_curados));
 
         nuevosFallecidos = root.findViewById(R.id.nuevosFallecidos);
-        nuevosFallecidos.setText(dto.nuevos_fallecidos);
+        nuevosFallecidos.setText(Integer.toString(dto.nuevos_fallecidos));
 
         nuevosEnUCI = root.findViewById(R.id.nuevosEnUCI);
-        nuevosEnUCI.setText(dto.nuevos_uci);
+        nuevosEnUCI.setText(Integer.toString(dto.nuevos_uci));
 
         return root;
     }
